81070ba8163a79a2b07aef286ad6ed34ec2d7cff
Johannes Graeter <johannes.graeter@kit.edu>
2018-12-04 18:45:39 +0100

    Plots, but not in gui yet.

--- src/eval_motion_gui.py
+++ src/eval_motion_gui.py
@@ -10,11 +10,55 @@ from e2eflow.util import config_dict
 
 FLAGS = eval_gui.FLAGS
 
-def track_points(flow, image):
-    pass
+import cv2
+import numpy as np
 
-def draw_tracked_points(flow1, image1):
-    raise Exception("Implement")
+
+def track_points(flows, images):
+    """
+    Track points in an image sequence as in the KLT tracker but with learned flow.
+    :param flows: estimated flow, shape(sequ_length,height,width,2)
+    :param images: input images for corner extraciton shape(sequ_length,height,width,3)
+    :return: tracklets with tracked feature points; np.array, with shape(sequ_length, maxCorners,2).
+            Tracklet length is always smaller/equal sequence length
+    """
+
+    # Caclucate points of interest in first image.
+    # params for ShiTomasi corner detection
+    feature_params = dict(maxCorners=100,
+                          qualityLevel=0.3,
+                          minDistance=7,
+                          blockSize=7)
+    # Points has shape (num_features, 1, 2)
+    points = cv2.goodFeaturesToTrack(cv2.cvtColor(images[0, :, :, :], cv2.COLOR_RGB2GRAY), mask=None, **feature_params)
+    points = np.squeeze(np.array(points))  # shape (num_features,2)
+
+    out = [points]
+
+    for flow in flows:
+        dudv = []
+        for u, v in points.astype(int):
+            du, dv = flow[v, u]
+            dudv.append((du, dv))
+        dudv = np.array(dudv)
+        points = points - dudv
+        out.append(points)
+        # print(points)
+
+    return out
+
+
+def draw_tracked_points(tracked_points, image1):
+    colors = np.random.randint(0, 255, (len(tracked_points[0]), 3))
+    # draw the tracks
+    mask = np.zeros_like(image1)
+    for i in range(1, len(tracked_points) - 1):
+        concat = np.concatenate((tracked_points[i - 1], tracked_points[i]), axis=1)
+        for num, (u_last, v_last, u, v) in enumerate(concat):
+            mask = cv2.line(mask, (u_last, v_last), (u, v), colors[num].tolist(), 1)
+            image1 = cv2.circle(image1, (u, v), 1, colors[num].tolist(), -1)
+    img = cv2.add(image1, mask)
+    return img
 
 
 def draw_motion(motion):
@@ -40,16 +84,29 @@ def main(argv=None):
     input_fn = lambda: input_fn0(needs_crop=True, center_crop=True, seed=None, swap_images=False)
 
     results = []
-    from matplotlib import pyplot as plt
-
+    import numpy as np
     for name in FLAGS.ex.split(','):
         # Here we get eval images, names and the estimated flow per iteration.
         # This should be sequences.
         display_images, image_names, flows = eval_gui._evaluate_experiment(name, input_fn, data_input)
-        #plt.imshow(flows[0])
-        print(flows[0].shape.as_list(), display_images[-1].shape.as_list())
-        tracked_points = track_points(flows[0], display_images[-1])
-        # motion_top_view = draw_motion(motion)
+
+        # Get flow for tracking features.
+        flows = np.squeeze(np.array(flows), axis=1)
+
+        # Get first images for corner extraction.
+        imgs = np.squeeze(np.array(display_images[-1]), axis=1)
+
+        tracked_points = track_points(flows, imgs)
+        img = draw_tracked_points(tracked_points, imgs[0, :, :])
+
+
+        # Todo: integrate in gui
+        from matplotlib import pyplot as plt
+        plt.imshow(img)
+        display_images.extend(img)
+
+        image_names.extend("tracklets")
+
         results.append(display_images)
 
     display(results, image_names)
